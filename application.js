d8bcc9f1b2cffc3f998ec2de7be71e0f8f86308e
Akash Chouhan <akash.c1500@gmail.com>
2021-12-30 03:06:19 +0530

    application setup

--- application.js
+++ application.js
@@ -5,8 +5,14 @@ const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
 const PORT = process.env.PORT || 5000;
+
+const {connectDB, DBConnectionStatus} = require('./config/Database');
+connectDB();
+
 app.use(cors());
 app.use(express.json())
+
+
 
 app.get('/', (req,res)=>{
     res.status(200).json({
@@ -15,13 +21,14 @@ app.get('/', (req,res)=>{
 })
 
 const problemSchedulerRoute = require('./routes/problemScheduler.route');
-// app.use('/api', problemSchedulerRoute)
+app.use('/api', problemSchedulerRoute)
 
 app.listen(PORT, (req,res)=>{
     const LOG={
         PORT: PORT,
         Status: `Server is Live`,
-        BaseURI: `localhost:${PORT}`
+        BaseURI: `localhost:${PORT}`,
+        DB_Status: DBConnectionStatus? "Remote DB (Atlas)": "Local DB"
     }
     console.table(LOG);
 })
